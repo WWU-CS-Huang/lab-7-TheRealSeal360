429201b5c9afa9ddb2938b028a8a4b67659ca935
TheRealSeal360 <sealj@wwu.edu>
2024-03-11 11:02:28 -0700

    initialized gradle and set up main function

---
+++ app/src/main/java/lab7/Huffman.java
@@ -0,0 +1,16 @@
+/*
+ * Jackson Seal and Kai Russell
+ */
+package lab7;
+import java.io.File;
+import java.util.Scanner;
+
+public class Huffman {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        File file = new File(args[0]);
+
+        Scanner sc = new Scanner(file);
+    }
+}
